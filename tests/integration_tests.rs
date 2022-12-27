76531381167c1ac25c62b4abe1fe1a53b83e6158
Christof Laenzlinger <laenzlinger@github.com>
2022-12-27 23:14:21 +0100

    Move test data into subdirectory

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -3,8 +3,8 @@ use irgen;
 #[test]
 fn test_generator() {
     let result = irgen::generate_from_wav(
-        String::from("tests/gibson.wav"),
-        String::from("tests/out.wav"),
+        String::from("tests/data/gibson.wav"),
+        String::from("tests/data/out.wav"),
     );
     assert_eq!(result, 16560);
 }
